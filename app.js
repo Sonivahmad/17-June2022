56f28ed727d8f12e4b3143a1bf1ace8cb08b6411
soniv ahmad <sonivahmad@gmail.com>
2022-06-17 20:09:41 +0530

    ForAndWhile

--- app.js
+++ app.js
@@ -1,6 +1,59 @@
-let a = process.argv[2]
-let i=a;
-while(i>=0){
-  console.log(i)
-  i--
-}
+ reverse
+ let a = parseInt(process.argv[2]);
+ let b = parseInt(process.argv[3]);
+ for(let i = b; i >= a ; i--){
+     console.log(i);
+ }
+
+
+
+ natural  umbers
+let a = parseInt(process.argv[2]);
+
+ for ( let i = 1; i<=a ; i++){
+    console.log(i);
+ }
+
+
+ even Number
+
+
+let a = parseInt(process.argv[2]);
+ for ( i = 0 ; i <= a; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+ }
+
+
+
+
+using while loop#
+even numbers
+ let a = parseInt(process.argv[2]);
+ let i=0;
+ while(i <= a){
+     if (i%2==0){
+         console.log(i);
+     }
+     i++
+    }
+
+
+
+
+
+
+reverse
+ let a = parseInt(process.argv[2]);
+ let b = parseInt(process.argv[3]);
+
+
+
+ natural numbers
+ let a = parseInt(process.argv[2]);
+ let i = 1;
+ while(i <= a){
+     console.log(i);
+     i++
+ }
